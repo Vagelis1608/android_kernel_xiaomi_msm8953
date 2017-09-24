296527d565faddebefaa1039d48832236b739a7d
NATO66613 <normandija1945@gmail.com>
2017-09-24 22:03:24 +0000

    Server TC

--- build.sh
+++ build.sh
@@ -31,7 +31,7 @@ echo -e "***********************************************$nocol"
 LC_ALL=C date +%Y-%m-%d
 kernel_dir=$PWD
 build=$kernel_dir/out
-export CROSS_COMPILE=~/Android/Kernel/toolchains/aarch64-linaro-7.x/bin/aarch64-linaro-linux-android-
+export CROSS_COMPILE=~/kernel/aarch64-linaro-7.x/bin/aarch64-linaro-linux-android-
 kernel="ReVolt"
 version="R8.3"
 vendor="xiaomi"
